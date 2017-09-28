7f3f007311b83a9eda561e1e23c8de56ddf37136
Carlos Hugo Rodriguez Calderon <allenwalker123.hr@gmail.com>
2017-09-27 22:51:05 -0500

    CracionDeProyecto
    se creo el proyecto con layouts para distintos tamaños de pantalla y soporte para idiomas

---
+++ Tareasoporte/app/src/main/java/com/example/azaa0/tareasoporte/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.azaa0.tareasoporte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
