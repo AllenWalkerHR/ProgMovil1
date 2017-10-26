e83dad08df2700f2071361f95c8c3364c0525bd4
Carlos Hugo Rodriguez Calderon <allenwalker123.hr@gmail.com>
2017-10-25 21:39:50 -0500

    Notas

---
+++ Notas/app/src/main/java/com/example/azaa0/notas/model/ADAdapter.java
@@ -0,0 +1,41 @@
+package com.example.azaa0.notas.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by SERVIDOR on 25/10/2016.
+ */
+
+public class ADAdapter extends SQLiteOpenHelper {
+    private final int DB_VERSION=1;
+    private final String  SCRIPT_DB = "create table NotaTarea (" +
+            "_id integer primary key, titulo text not null," +
+            "descripcion text, tipo integer, " +
+            "fecha_hora text," +
+            "fecha_hora_vencimineto integer); " +
+            "create table multimedia (" +
+            "_idNotaTarea  integer references NotaTarea(_id), " +
+            "titulo text, " +
+            "tipo integer not null, " +
+            "uri text not null" +
+            ");" +
+            "create table recordatorios (_idNotaTarea integer reference NotaTarea(_id)," +
+            "fecha_hora) text  not null";
+
+    public ADAdapter(Context context, int version) {
+
+        super(context, "db", null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SCRIPT_DB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ Notas/app/src/main/java/com/example/azaa0/notas/model/NotaTarea.java
@@ -0,0 +1,85 @@
+package com.example.azaa0.notas.model;
+
+import java.util.Date;
+
+/**
+ * Created by alcohonsilver on 27/10/16.
+ */
+public class NotaTarea {
+    int id;
+    String titulo;
+    String descripcion;
+    int tipo;
+    String hora_fecha;
+
+    public NotaTarea() {
+
+    }
+
+
+    public int getHora_fecha_vencimiento() {
+        return hora_fecha_vencimiento;
+    }
+
+    public void setHora_fecha_vencimiento(int hora_fecha_vencimiento) {
+        this.hora_fecha_vencimiento = hora_fecha_vencimiento;
+    }
+
+    private int hora_fecha_vencimiento;
+
+    public NotaTarea(int id, String titulo, String descripcion,
+                     int tipo, String hora_fecha, int hora_fecha_vencimiento) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.hora_fecha = hora_fecha;
+        this.hora_fecha_vencimiento = hora_fecha_vencimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getHora_fecha() {
+        return hora_fecha;
+    }
+
+    public void setHora_fecha(String hora_fecha) {
+        this.hora_fecha = hora_fecha;
+    }
+
+
+
+
+
+
+}
